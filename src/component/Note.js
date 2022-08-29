b317f94d5a18bff8fc94dbd48288ff0cecff646c
joycethh <joycethh@outlook.com>
2022-08-29 14:28:13 -0500

    delete/DELETE method works

--- src/component/Note.js
+++ src/component/Note.js
@@ -1,11 +1,21 @@
 import React from "react";
+import axios from "axios";
 
 const Note = ({ noteItem }) => {
+  const handleClick = async () => {
+    const response = await axios.delete(
+      `http://localhost:4000/${noteItem._id}`
+    );
+    if (response.status === 200) {
+      console.log("the note is deleted");
+    }
+  };
+
   return (
     <div className="note">
       <h1>{noteItem.title}</h1>
       <p>{noteItem.content}</p>
-      <button>Delete</button>
+      <button onClick={handleClick}>Delete</button>
     </div>
   );
 };
