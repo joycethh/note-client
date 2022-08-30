8ed0c51c9cb877e0ab720c376ba3913498e48c82
joycethh <joycethh@outlook.com>
2022-08-29 20:04:18 -0500

    testing: use useContext directly (instead of custmized hook) works as well

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import axios from "axios";
 
-import { useNotesContext } from "./component/context/useNoteContext";
+// import { useNotesContext } from "./component/context/useNoteContext";
+import { NotesContext } from "./component/context/NotesContext";
 
 import Header from "./component/Header";
 import Footer from "./component/Footer";
@@ -9,15 +10,13 @@ import Note from "./component/Note";
 import CreateArea from "./component/CreateArea";
 
 const App = () => {
-  const { notes, dispatch } = useNotesContext();
-
-  // const [notes, setNotes] = useState([]);
+  // const { notes, dispatch } = useNotesContext();
+  const { notes, dispatch } = useContext(NotesContext);
 
   useEffect(() => {
     const fetchNotes = async () => {
       const response = await axios.get("http://localhost:4000/");
       const data = await response.data;
-      // setNotes(data);
       dispatch({ type: "FETCH", payload: data });
     };
 
