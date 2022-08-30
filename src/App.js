bf49759de37e1120e4446578d52d8cbfbd60f625
joycethh <joycethh@outlook.com>
2022-08-29 20:00:50 -0500

    GET all method works in component level

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
+
+import { useNotesContext } from "./component/context/useNoteContext";
+
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Note from "./component/Note";
 import CreateArea from "./component/CreateArea";
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
+  const { notes, dispatch } = useNotesContext();
+
+  // const [notes, setNotes] = useState([]);
+
   useEffect(() => {
     const fetchNotes = async () => {
       const response = await axios.get("http://localhost:4000/");
       const data = await response.data;
-      setNotes(data);
+      // setNotes(data);
+      dispatch({ type: "FETCH", payload: data });
     };
 
     fetchNotes();
-  }, []);
+  }, [dispatch]);
 
   return (
     <div>
