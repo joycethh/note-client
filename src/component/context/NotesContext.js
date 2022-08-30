a060fb417e41a2ec4d52482a403e4bde1ad75544
joycethh <joycethh@outlook.com>
2022-08-29 19:58:13 -0500

    create notes context, context provider and warp the root  with context provider

--- src/component/context/NotesContext.js
+++ src/component/context/NotesContext.js
@@ -1,7 +1,31 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
+//create notes context
 export const NotesContext = createContext();
 
-export const NoteContextProvider = () => {
-  return <NotesContext.Provider></NotesContext.Provider>;
+//create note reducer
+export const noteReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH":
+      return {
+        notes: action.payload,
+      };
+    case "CREATE":
+      return [...state, action.payload];
+    case "DELETE":
+      return state.filter((note) => note._id !== action.payload); // id?
+    default:
+      return state;
+  }
+};
+
+//context provider
+export const NoteContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(noteReducer, { notes: null }); //reducer, initialValue
+
+  return (
+    <NotesContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </NotesContext.Provider>
+  );
 };
